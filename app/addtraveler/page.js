b83481ee587133cca1d3dc225ecb8b3b0244529a
Farhan Asif <farhanmatics@gmail.com>
2024-01-28 13:49:27 +0000

    folder

---
+++ app/addtraveler/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Add = () => {
+  return (
+    <div>Add Traveler</div>
+  )
+}
+
+export default Add
---
+++ app/traveler/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Traveler = () => {
+  return (
+    <div>Traveler</div>
+  )
+}
+
+export default Traveler
