c13b61472e0526a181b494da43d238af642f7dea
Farhan Asif <145195023+farhanmatics@users.noreply.github.com>
2024-01-28 23:19:19 +0000

    add traveler fix

--- app/addtraveler/page.js
+++ app/addtraveler/page.js
@@ -1,24 +1,78 @@
-import PageBottom from '@/components/pageBottom'
-import PageHeader from '@/components/pageHeader'
-import React from 'react'
-import Link from 'next/link'
+import PageBottom from "@/components/pageBottom";
+import PageHeader from "@/components/pageHeader";
+import React from "react";
+import Link from "next/link";
 
 const Add = () => {
   return (
     <>
-      <PageHeader backlink='/traveler'/>
-      <div className='pt-20'>
-        <div className="flex items-center justify-center">
-          <div
-              className="flex flex-col border p-12 shadow-lg shadow-purple-200 rounded-md border-purple-50">
-              <p className="py-3 font-semibold capitalize text-tourPurple">Add Traveler :</p>
+      <PageHeader backlink="/traveler" />
+      <div className="pt-20 px-4">
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex flex-col border p-4 rounded-md border-purple-800">
+            <p className="py-1 font-semibold capitalize text-tourPurple">
+              Add a New Traveler
+            </p>
+            <div className="mt-2">
+              <label htmlFor="FirstName" className="text-lightPurple">
+                {" "}
+                First Name
+              </label>
+              <input
+                type="text"
+                name="FirstName"
+                id=""
+                className="form-input px-2 border-2 border-tourPurple w-full rounded-md"
+              />
+            </div>
+            <div className="mt-2">
+              <label htmlFor="SurName" className="text-lightPurple">
+                {" "}
+                SurName
+              </label>
+              <input
+                type="text"
+                name="SurName"
+                id=""
+                className="form-input px-2 border-2 border-lightPurple w-full rounded-md"
+              />
+            </div>
+            <div className="mt-2">
+              <label htmlFor="SurName" className="text-lightPurple">
+                Passport No:
+              </label>
+              <input
+                type="text"
+                name="Passport"
+                id=""
+                className="form-input px-2 border-2 border-lightPurple w-full rounded-md"
+              />
+            </div>
+            <div className="mt-2">
+              <label htmlFor="SurName" className="text-lightPurple">
+                Validity:
+              </label>
+              <input
+                type="date"
+                name="Passport"
+                id=""
+                className="form-input px-2 border-2 border-lightPurple w-full rounded-md"
+              />
+            </div>
           </div>
+          <div className="px-4 pt-4">
+            <Link
+              href="#"
+              className="bg-tourPurple text-white px-4 py-3 rounded-md uppercase font-semibold text-sm"
+            >
+              use Simple Passport Scan
+            </Link>
+          </div>
+        </div>
       </div>
-      </div>
-      <PageBottom title='Add Traveler' linkurl='/traveler' />
+      <PageBottom title="Add Traveler" linkurl="/traveler" />
     </>
-    
-  )
-}
+  );
+};
 
-export default Add
+export default Add;
--- app/confirmation/page.js
+++ app/confirmation/page.js
@@ -1,24 +1,50 @@
-import PageBottom from '@/components/pageBottom'
-import PageHeader from '@/components/pageHeader'
-import React from 'react'
-import Link from 'next/link'
+import PageBottom from "@/components/pageBottom";
+import PageHeader from "@/components/pageHeader";
+import React from "react";
+import Link from "next/link";
 
 const Confirmation = () => {
   return (
     <>
-      <PageHeader backlink='/'/>
-      <div className='pt-20'>
-        <div className="flex items-center justify-center">
-          <div
-              className="flex flex-col border p-12 shadow-lg shadow-purple-200 rounded-md border-purple-50">
-              <p className="py-3 font-semibold capitalize text-tourPurple">Thanks :</p>
-          </div>
+      <div className="bg-tourPurple flex flex-col h-screen items-center justify-center">
+        <svg
+          xmlns="https://www.w3.org/2000/svg"
+          width="60"
+          height="60"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-badge-check text-white animate-bounce"
+        >
+          <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+          <path d="m9 12 2 2 4-4" />
+        </svg>
+        <p className="text-white text-xl px-2 py-2">
+          Your booking has been confirmed
+        </p>
+        <p className="text-white text-xl px-2 py-2">
+          Booking ID: <span className="font-bold">IKL4P0KN55</span>
+        </p>
+        <div className="flex items-center justify-between">
+          <a
+            href="/"
+            className="text-white text-lg px-2 py-2 underline font-semibold"
+          >
+            Home
+          </a>
+          <a
+            href="/"
+            className="text-white text-lg px-2 py-2 underline font-semibold"
+          >
+            Track Status
+          </a>
+        </div>
       </div>
-      </div>
-      <PageBottom title='Home' linkurl='/' />
     </>
-    
-  )
-}
+  );
+};
 
-export default Confirmation
+export default Confirmation;
