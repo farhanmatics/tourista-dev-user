efb06600e913ab8a3d1fd98d4e5abfa49085077f
Tausif Fardin <tausiffardin4@gmail.com>
2024-02-13 23:41:21 +0600

    design issue fix for ios

--- app/details/page.js
+++ app/details/page.js
@@ -1,15 +1,26 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import PageBottom from "../../components/pageBottom";
 import PageHeader from "../../components/pageHeader";
 import { useAppContext } from "../../contexts/AppContext";
-
 const Details = () => {
+  const [defaultDate, setDefaultDate] = useState("");
   const { setTravelerInfo } = useAppContext();
+
+  useEffect(() => {
+    const today = new Date().toISOString().split("T")[0];
+    setDefaultDate(today);
+  }, []);
 
   const handleDateChange = (event) => {
     const selectedDate = event.target.value;
-    setTravelerInfo((prevInfo) => ({ ...prevInfo, date: selectedDate }));
+    setDefaultDate(selectedDate);
+    setTravelerInfo((prevInfo) => ({
+      ...prevInfo,
+      date: selectedDate,
+      visaType: "6",
+    }));
   };
 
   const handleVisaTypeChange = (event) => {
@@ -24,36 +35,68 @@ const Details = () => {
     <>
       <PageHeader backlink="/" />
       <div className="pt-20">
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center ">
           <p className="py-3 font-semibold capitalize text-darkSky">
             <span className="uppercase font-bold text-lg">Thailand</span>
           </p>
         </div>
-        
-        <div className="flex items-center justify-center">
+
+        <div className="flex items-center justify-center ">
           <div className="flex flex-col bg-gray-200 rounded-lg px-20 py-3 mt-10 pb-4">
-            <div className="flex flex-col px-2">
-              <h1 className="text-darkSky py-2 font-semibold flex">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-calendar"><path d="M8 2v4"/><path d="M16 2v4"/><rect width="18" height="18" x="3" y="4" rx="2"/><path d="M3 10h18"/></svg>
+            <div className="flex flex-col px-2 w-full">
+              <h1 className="text-darkSky py-2 font-semibold flex justify-start items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="17"
+                  height="17"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-calendar"
+                >
+                  <path d="M8 2v4" />
+                  <path d="M16 2v4" />
+                  <rect width="18" height="18" x="3" y="4" rx="2" />
+                  <path d="M3 10h18" />
+                </svg>
                 <span className="pl-2">Date of Entry</span>
               </h1>
               <input
                 type="date"
-                name=""
-                id=""
+                name="date"
+                id="date"
+                value={defaultDate}
                 onChange={handleDateChange}
-                className="form-input text-darkSky border-darkSky rounded border-2 px-4 py-2 text-sm w-auto"
+                className="text-darkSky border-darkSky rounded border-2 px-4 py-2 text-sm w-full"
               />
             </div>
             <div className="flex flex-col px-2 mb-4 mt-2">
-            <h1 className="text-darkSky py-2 font-semibold flex">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-file-check"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="m9 15 2 2 4-4"/></svg>
+              <h1 className="text-darkSky py-2 font-semibold flex justify-start items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="17"
+                  height="17"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-file-check"
+                >
+                  <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
+                  <path d="M14 2v4a2 2 0 0 0 2 2h4" />
+                  <path d="m9 15 2 2 4-4" />
+                </svg>
                 <span className="pl-2">Visa Type</span>
               </h1>
               <select
-                name=""
-                id=""
-                className="form-select text-darkSky border-darkSky rounded border-2 px-4 py-2 text-sm"
+                name="visa"
+                id="visa"
+                className="text-darkSky border-darkSky rounded border-2 px-4 py-2 text-sm w-full"
                 onChange={handleVisaTypeChange}
               >
                 <option value="6">Tourist</option>
